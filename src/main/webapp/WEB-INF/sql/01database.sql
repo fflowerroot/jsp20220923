6209bfea906a70ac47da2c4ddd0dd440a6b25f1d
fflowerroot <108855805+fflowerroot@users.noreply.github.com>
2022-10-11 21:49:19 +0900

    ,

---
+++ src/main/webapp/WEB-INF/sql/01database.sql
@@ -0,0 +1,18 @@
+-- Customers 테이블 내용보기
+SELECT * FROM Customers;
+SELECT * FROM Employees;
+select Firstname, lastname, birthdate from Employees;
+select distinct country, city from Customers;
+select country from Customers;
+SELECT count(*) FROM Customers;
+SELECT DISTINCT Country FROM Suppliers;
+SELECT count(DISTINCT Country) FROM Suppliers;
+-- table 의 하나의 행(row)을 record(tuple)
+-- table 의 각 행은 같은 속성(attribute, field, column)을 갖는다
+
+-- 한 줄 주석
+/*
+여러 줄 주석
+*/
+
+ 
